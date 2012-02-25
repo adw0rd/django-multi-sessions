e706a5c75b68ed2fc8afc2a6863b0c5f35e9ebbf
Mikhail Andreev <x11org@gmail.com>
2012-02-26 01:48:52 +0400

    Update description for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-multi-sessions',
     version='0.0.1',
-    description="Multi Session Backend For Django",
+    description="Multi-sessions backend for Django",
     long_description="",
     keywords='django, sessions,',
     author='Mikhail Andreev',
