861dbc48824d328b6eb60830d45579cca69df3cf
Mikhail Andreev (aka adw0rd) <x11org@gmail.com>
2012-02-27 12:56:10 +0400

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-multi-sessions',
-    version='0.0.1',
+    version='0.0.2',
     description="Multi-sessions backend for Django",
     long_description="",
     keywords='django, sessions',
